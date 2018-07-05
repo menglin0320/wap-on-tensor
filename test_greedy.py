ad285f0beb8dabfd746015c67267128c0695c77f
menglin0320 <1372920944@qq.com>
2018-07-05 12:30:23 -0400

    test if eval is just different from train

--- test_greedy.py
+++ test_greedy.py
@@ -101,7 +101,7 @@ class test_code:
         Words, Alphas, height, width, Beta = sess.run(
             [self.logits, self.alpha_t, model.feature_height, model.feature_width, self.beta_t],
             feed_dict={model.x: x, model.x_mask: x_mask,
-                       model.is_train: True})
+                       model.is_train: False})
 
         Words = [w[0] for w in Words]
         str_list = []
